b902effe7d91f6669dcf7defd466dd73182b1d75
mubaihaqi <muhammadumarbaihaqi93@student.cs.unida.gontor.ac.id>
2025-05-04 15:34:24 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <button className="bg-primary-500 text-white p-4 rounded border-teal-400 border">
+        Test Button
+      </button>
+    </>
+  );
+}
