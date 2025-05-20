4f2e3dac79232aeb6c091cdff22e62227659f403
mubaihaqi <muhammadumarbaihaqi93@student.cs.unida.gontor.ac.id>
2025-05-20 10:58:50 +0700

    Adding docker configuration to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "flowbite";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
